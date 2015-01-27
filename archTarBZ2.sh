8a0ecc0fd0de7661be9ca5b130e0e858541761db
Matt DePorter <nfaction@gmail.com>
2015-01-27 15:13:30 -0700

    Added block statments with timestamp and timestamped logfile at destination

--- archTarBZ2.sh
+++ archTarBZ2.sh
@@ -8,7 +8,7 @@ usage()
     exit 2
 }
 
-( if [ $# = 0 ]; then
+if [ $# = 0 ]; then
 	usage
 	
 elif [ $1 == "-h" ] || [ $1 == "--help" ]; then
@@ -18,8 +18,8 @@ elif [ $# -gt 3 ]; then
     usage
 
 else
+	( echo $0 starting at $(date)
 	startTime=$(date +%s);
-	echo "==== Starting ============================"
 	
 	# Set source and destination vars
 	src_dir=$1
@@ -37,10 +37,10 @@ else
 	# grab bite sizes from src dir
 	echo "==== Building File List =================="
 	du -s ${src_dir}* > /tmp/dus
-	echo "Done" 
+	echo "==== Building File List Complete ==================" 
 
 	# read line by line
-	echo "==== Taring Files ========================"
+	echo "==== Begin Taring All Files ========================"
 	while read line
 	do
 		# split into file size and name
@@ -50,21 +50,21 @@ else
 
 		if [ "$size" -lt "$threshBytes" ]; then
 			# tar the small file to destination
-			echo "${fileName} Taring at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "==== ${fileName} Taring Started at $(date +%Y\/%m\/%d\ %H\:%M)"
 			tar -jcf ${dst_dir}${fileName}.tar.bz2 ${filePath}
-			echo "-Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "==== ${fileName} Taring Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
 
 		else 
 			# split tar the big files
-			echo "${fileName} Split Taring at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "==== ${fileName} Split Taring Starting at $(date +%Y\/%m\/%d\ %H\:%M)"
 			echo tar -jcf - ${filePath} | split -b $chunkSize - ${dst_dir}${fileName}_parts.tar.bz2
-			echo "-Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "==== ${fileName} Split Taring Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
 
 		fi
 
 	done < "/tmp/dus"
 	
-	echo "==== All tars complete ====================" 
+	echo "==== All tars completed at $(date +%Y\/%m\/%d\ %H\:%M) ====================" 
 
 	# remove tmp file
 	rm -rf /tmp/dus 
@@ -76,4 +76,4 @@ else
 	echo "Total Runtime: $(($diff / 60)) minutes and $(($diff % 60)) seconds."
 
 
-fi ) >& log
+fi ) >& $dst_dir/tar-split-log.$(date +%Y%m%d.%H%M)
