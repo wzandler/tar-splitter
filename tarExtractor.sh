48b92c9a073c6c5fe0bdb1f924d9b2a2c55d5c24
wzandler <wjzandler@gmail.com>
2015-02-04 11:08:15 -0700

    Added extract command

--- tarExtractor.sh
+++ tarExtractor.sh
@@ -2,3 +2,26 @@
 
 // Take tar file from input and unarchive to destination
 
+
+usage() 
+{
+    echo "Usage: $0 [-h] <extraction_file> <destination_directory/>"
+    exit 2
+}
+
+if [ $# = 0 ]; then
+	usage
+	exit 0
+fi
+	
+if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+    usage
+    exit 0	
+fi
+
+ext_file=$1
+dst_dir=$2
+
+
+
+cat ${ext_file}_parts.tar.bz2* | tar -jxf - -C ${dst_dir}
