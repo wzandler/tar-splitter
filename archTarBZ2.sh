a4d03c86f6ce24bee929a5c3c25a2b6ad99677ba
wzandler <wjzandler@gmail.com>
2015-02-19 13:33:35 -0700

    more format changes

--- archTarBZ2.sh
+++ archTarBZ2.sh
@@ -37,19 +37,19 @@ startTime=$(date +%s);
 
 
 # grab byte sizes from src dir
-echo "==== Building File List =================="
+echo "++++ Building File List ++++++++++++++++++++++++++++"
 du -sb ${src_dir}* > /tmp/dus
-echo "**** Building File List Complete *********" 
+echo "==== Building File List Complete ===================" 
 
 # read line by line
-echo "==== Begin Taring All Files ========================"
+echo "**** Begin Taring All Files ************************"
 while read line
 do
 	# split into file size and name
 	size=$(echo $line | cut -f 1 -d ' ' )
 	filePath=$(echo $line | cut -f 2- -d ' ')
 	fileName=$(echo $filePath | rev | cut -f 1 -d '/' | rev)
-	echo 
+	# echo 
 	# if block check 
 		
 	if [ -f  ${dst_dir}${fileName}.tar.bz2 ] || [ -f ${dst_dir}${filePath}_parts.tar.bz2aa ]; then
@@ -62,13 +62,13 @@ do
 			# tar the small file to destination
 			echo "++++ ${fileName} Taring Started at $(date +%Y\/%m\/%d\ %H\:%M)"
 			tar -jcf ${dst_dir}${fileName}.tar.bz2 ${filePath}
-			echo "**** ${fileName} Taring Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "==== ${fileName} Taring Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
 
 		else 
 			# split tar the big files
-			echo "==== ${fileName} Split Taring Starting at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "++++ ${fileName} Split Taring Starting at $(date +%Y\/%m\/%d\ %H\:%M)"
 			tar -jcf - ${filePath} | split -b $chunkSize - ${dst_dir}${fileName}_parts.tar.bz2
-			echo "**** ${fileName} Split Taring Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
+			echo "==== ${fileName} Split Taring Finished at $(date +%Y\/%m\/%d\ %H\:%M)"
 
 		fi
 
